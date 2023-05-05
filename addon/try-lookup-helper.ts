d1cfcd04f4c9727b2435cb4f80edaadb8cd69713
Krystan HuffMenne <krystan@tilde.io>
2023-05-05 10:45:27 -0700

    Apply ember 4.12 `ember addon` diff (as applicable)

--- addon/try-lookup-helper.ts
+++ addon/try-lookup-helper.ts
@@ -1,5 +1,4 @@
 import Helper from '@ember/component/helper';
-import { type Factory } from '@ember/owner';
 import { getOwner } from './get-owner';
 import { assertExists } from './types/util';
 
@@ -9,7 +8,10 @@ interface TryLookupHelperSignature {
   Args: {
     Positional: Positional;
   };
-  Return: Factory<object> | undefined;
+  // NOTE: Actual type is `Factory<object> | undefined`, but due to
+  // discrepancies in the Factory type between the 4.8 and 4.10 Ember types, we
+  // can't use that type here.
+  Return: unknown;
 }
 
 /**
@@ -19,7 +21,7 @@ interface TryLookupHelperSignature {
  * this-fallback invocations.
  */
 export default class TryLookupHelper extends Helper<TryLookupHelperSignature> {
-  compute([name]: Positional): Factory<object> | undefined {
+  compute([name]: Positional): unknown {
     const owner = assertExists(getOwner(this), 'Could not find owner');
     return owner.factoryFor(`helper:${name}`)?.class;
   }
