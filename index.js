04f02d3d8827b64f2108bee41b6ab8cb3a17cbef
David Taylor <david@taylorhq.com>
2023-05-10 11:15:37 +0100

    Allow babel transformation to parallelized
    When the `ember-cli-babel.throwUnlessParallelizable` flag was enabled, this addon would cause an error to be raised like
    
    ```
    [broccoli-persistent-filter:Babel > [Babel: ember-this-fallback]: Babel: ember-this-fallback] was configured to `throwUnlessParallelizable` and was unable to parallelize a plugin.
    ```
    
    This commit updates the plugin definition to be compatible with parallel transformations, and adds the `throwUnlessParallelizable` flag to the dummy app. This change also seems to resolve the 'opting out of caching' error which previously required a `baseDir` hack.
    
    More info on parallelization at https://github.com/babel/ember-cli-babel#parallel-builds and https://github.com/babel/broccoli-babel-transpiler#parallel-transpilation

--- index.js
+++ index.js
@@ -8,16 +8,10 @@ module.exports = {
   },
 
   _buildPlugin() {
-    const ThisFallbackPlugin = require('./lib/this-fallback-plugin');
-
-    // HACK: Seems strange that this is necessary, but without it we get:
-    // broccoli-babel-transpiler is opting out of caching due to a plugin that does not provide a caching strategy
-    ThisFallbackPlugin.baseDir = () => __dirname;
-
     return {
       name: 'ember-this-fallback',
       baseDir: () => __dirname,
-      plugin: ThisFallbackPlugin,
+      plugin: require.resolve('./lib/this-fallback-plugin'),
     };
   },
 };
